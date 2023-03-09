75ebee677cd1bb0e1e781c5052e31c6e1b134624
Bruno Castro <mcastrobruno@gmail.com>
2023-03-09 18:52:31 +0000

    moved get_chained_variable and set_dependent_variable to chaining.py file

--- chaining.py
+++ chaining.py
@@ -1,8 +1,13 @@
-from preprocess import *
-from embeddings import *
-from modelling.modelling import *
-from modelling.data_model import *
-import random
+import pandas as pd 
+import re
+from Config import *
+
+
+# set current iteration of chain type called from main() to programmes y dependent variable
+def set_dependent_variable(dependent_variable, df:pd.DataFrame) -> pd.DataFrame:
+    df["y"] = dependent_variable
+    df = df.loc[(df["y"] != '') & (~df["y"].isna()),]
+    return df
 
 # create chains that later are passed to set_dependent_variable() function i.e. to set y for each level of iteraction through the chain types
 def get_chained_variable(df:pd.DataFrame):
--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from preprocess import *
 from embeddings import *
 from modelling.modelling import *
 from modelling.data_model import *
-from get_chained_variable import *
-from set_dependent_variable import *
+from chaining import get_chained_variable, set_dependent_variable
 import random
 
 seed = 0
